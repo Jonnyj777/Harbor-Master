d789927f8b6bba476a2f9837e5ea4c16c8d17fda
Andrew Goldstein <fablefox5@gmail.com>
2025-09-26 16:36:54 -0400

    Mirror added and initial setup for lobby

---
+++ Port Authority/Assets/Multiplayer/Scripts/HostLobby.cs
@@ -0,0 +1,24 @@
+using Mirror;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HostLobby : MonoBehaviour
+{
+    [SerializeField]
+    private Button hostButton;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        hostButton.onClick.AddListener(() =>
+        {
+            NetworkManager.singleton.StartHost();
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Port Authority/Assets/Multiplayer/Scripts/NetworkLobby.cs
@@ -0,0 +1,29 @@
+using Mirror;
+using TMPro;
+using UnityEngine;
+
+public class NetworkLobby : NetworkRoomManager
+{
+
+    [SerializeField]
+    private Canvas lobbyCanvas;
+
+    [SerializeField]
+    private TextMeshProUGUI playerList;
+
+    public override void OnRoomServerAddPlayer(NetworkConnectionToClient conn) {
+        foreach(PendingPlayer player in pendingPlayers)
+        {
+            playerList.text += "\n" + player;
+
+        }
+    
+    }
+
+    public override void OnRoomClientEnter()
+    {
+        base.OnRoomClientEnter();
+        lobbyCanvas.enabled = true;
+    }
+
+}
