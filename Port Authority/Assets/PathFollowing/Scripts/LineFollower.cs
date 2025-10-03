fa3432297d3bb222d76e140d1fe98c915705902a
Nicolas Calafiore <121151093+NicolasCalafiore@users.noreply.github.com>
2025-10-03 11:34:16 -0400

    initial

--- Port Authority/Assets/PathFollowing/Scripts/LineFollower.cs
+++ Port Authority/Assets/PathFollowing/Scripts/LineFollower.cs
@@ -4,87 +4,90 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class LineFollower : MonoBehaviour
+namespace PathFollowing.Scripts
 {
-    public DrawLine drawControl;
-    public float speed = 5f;
-
-    bool startMovement = false;
-    Vector3[] positions;
-    int moveIndex = 0;
-
-    // draw line once the object is clicked
-    private void OnMouseDown()
+    public class LineFollower : MonoBehaviour
     {
-        drawControl.DeleteLine();
-        startMovement = false;
-        drawControl.StartLine(transform.position);
-    }
+        public DrawLine drawControl;
+        public float speed = 5f;
 
-    private void OnMouseDrag()
-    {
-        drawControl.UpdateLine();
-    }
+        bool startMovement = false;
+        Vector3[] positions;
+        int moveIndex = 0;
 
-    private void OnMouseUp()
-    {
-        positions = new Vector3[drawControl.drawLine.positionCount];
-        drawControl.drawLine.GetPositions(positions);
-        startMovement = true;
-        moveIndex = 0;
-    }
-
-    private void Update()
-    {
-        if (startMovement)
+        // draw line once the object is clicked
+        private void OnMouseDown()
         {
-            // update position and direction of object
-            Vector3 currentPos = positions[moveIndex];
-            transform.position = Vector3.MoveTowards(transform.position, currentPos, speed * Time.deltaTime);
+            drawControl.DeleteLine();
+            startMovement = false;
+            drawControl.StartLine(transform.position);
+        }
 
-            Vector3 direction = (currentPos - transform.position).normalized;
+        private void OnMouseDrag()
+        {
+            drawControl.UpdateLine();
+        }
 
-            if (direction.sqrMagnitude > 0.001f) 
+        private void OnMouseUp()
+        {
+            positions = new Vector3[drawControl.drawLine.positionCount];
+            drawControl.drawLine.GetPositions(positions);
+            startMovement = true;
+            moveIndex = 0;
+        }
+
+        private void Update()
+        {
+            if (startMovement)
             {
-                Quaternion targetRotation = Quaternion.LookRotation(direction);
-                float angleOffset = Quaternion.Angle(transform.rotation, targetRotation);
+                // update position and direction of object
+                Vector3 currentPos = positions[moveIndex];
+                transform.position = Vector3.MoveTowards(transform.position, currentPos, speed * Time.deltaTime);
 
-                if (angleOffset > 5f)
+                Vector3 direction = (currentPos - transform.position).normalized;
+
+                if (direction.sqrMagnitude > 0.001f)
                 {
-                    transform.LookAt(currentPos);
+                    Quaternion targetRotation = Quaternion.LookRotation(direction);
+                    float angleOffset = Quaternion.Angle(transform.rotation, targetRotation);
+
+                    if (angleOffset > 5f)
+                    {
+                        transform.LookAt(currentPos);
+                    }
                 }
-            }
 
-            float distance = Vector3.Distance(currentPos, transform.position);
-            if (distance <= 0.05f)
-            {
-                moveIndex++;
-            }
-
-            // remove the part of line already traveled on
-            if (drawControl.drawLine.positionCount > 1)
-            {
-                Vector3[] currentPositions = new Vector3[drawControl.drawLine.positionCount];
-                drawControl.drawLine.GetPositions(currentPositions);
-
-                currentPositions[0] = transform.position;
-
-                if (Vector3.Distance(currentPositions[0], currentPositions[1]) < 0.05f)
+                float distance = Vector3.Distance(currentPos, transform.position);
+                if (distance <= 0.05f)
                 {
-                    List<Vector3> temp = new List<Vector3>(currentPositions);
-                    temp.RemoveAt(0);
-                    currentPositions = temp.ToArray();
+                    moveIndex++;
                 }
 
-                drawControl.drawLine.positionCount = currentPositions.Length;
-                drawControl.drawLine.SetPositions(currentPositions);
-            }
+                // remove the part of line already traveled on
+                if (drawControl.drawLine.positionCount > 1)
+                {
+                    Vector3[] currentPositions = new Vector3[drawControl.drawLine.positionCount];
+                    drawControl.drawLine.GetPositions(currentPositions);
 
-            // remove line after following finishes
-            if (moveIndex > positions.Length - 1)
-            {
-                startMovement = false;
-                drawControl.DeleteLine();
+                    currentPositions[0] = transform.position;
+
+                    if (Vector3.Distance(currentPositions[0], currentPositions[1]) < 0.05f)
+                    {
+                        List<Vector3> temp = new List<Vector3>(currentPositions);
+                        temp.RemoveAt(0);
+                        currentPositions = temp.ToArray();
+                    }
+
+                    drawControl.drawLine.positionCount = currentPositions.Length;
+                    drawControl.drawLine.SetPositions(currentPositions);
+                }
+
+                // remove line after following finishes
+                if (moveIndex > positions.Length - 1)
+                {
+                    startMovement = false;
+                    drawControl.DeleteLine();
+                }
             }
         }
     }
