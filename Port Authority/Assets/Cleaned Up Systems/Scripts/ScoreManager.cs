497f52803cba7d799732cb43ebda84e1b4dda1db
Andrew Goldstein <fablefox5@gmail.com>
2025-10-22 03:21:39 -0400

    added hook for score changing with UI

--- Port Authority/Assets/Cleaned Up Systems/Scripts/ScoreManager.cs
+++ Port Authority/Assets/Cleaned Up Systems/Scripts/ScoreManager.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using Mirror;
+using static UnityEngine.PlayerLoop.EarlyUpdate;
+
 public class ScoreManager : NetworkBehaviour
 {
     public static ScoreManager Instance;
-    [SyncVar] private int score = 0;
+    [SyncVar(hook = nameof(OnScoreChanged))] private int score = 0;
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI popUpText;
+    public bool hasBonus = false;
 
     private void Start()
     {
@@ -20,13 +23,18 @@ public class ScoreManager : NetworkBehaviour
         Instance = this;
     }
 
-    [Server]
+
+    void OnScoreChanged(int oldVal, int newVal)
+    {
+        scoreText.text = "$ " + newVal;
+
+        StartCoroutine(ShowPopUp("+$ " + (newVal-oldVal), hasBonus));
+    }
+
     public void AddScore(int scoreUpdate, bool bonus)
     {
         score += scoreUpdate;
-        scoreText.text = "$ " + score;
-
-        StartCoroutine(ShowPopUp("+$ " + scoreUpdate, bonus));
+        hasBonus = bonus;
     }
     private IEnumerator ShowPopUp(string text, bool bonus)
     {
