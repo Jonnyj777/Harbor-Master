ffa89b0bd870c80099e943c4c258c1af76deeede
Andrew Goldstein <fablefox5@gmail.com>
2025-11-12 22:47:44 -0500

    attempt delete for readding

--- Port Authority/Assets/Game/Scripts/NewCargoType.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "NewCargoType", menuName = "Cargo/Cargo Type")]
-public class CargoType : ScriptableObject
-{
-    public string cargoName;
-    public Color color;
-    public int basePrice;
-}
