b5a8a1857312c3ef66c2078f64038de1b58c517d
sadew01 <93170603+sadew01@users.noreply.github.com>
2025-11-21 15:53:28 -0500

    Added fade transitions between scenes and fixed ui issues from showcase

--- Port Authority/Assets/Game/Scripts/SceneManagerScript.cs
+++ Port Authority/Assets/Game/Scripts/SceneManagerScript.cs
@@ -1,17 +1,82 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class SceneManagerScript : MonoBehaviour
 {
+    [SerializeField] private CanvasGroup canvasGroup;
+    [SerializeField] private float fadeDuration = 3f;
+    private Coroutine currentFade;
+    private bool fadeInStarted = false;
+    public TextMeshProUGUI loadingText;
+
+
+    private void Awake()
+    {
+        canvasGroup.gameObject.SetActive(true);
+    }
+
+    private void Start()
+    {
+        if (!fadeInStarted)
+        {
+            fadeInStarted = true;
+            StartCoroutine(DelayedFadeIn());
+        }
+    }
     public void ResetScene()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-        Time.timeScale = 1f;
+        StartCoroutine(LoadScene(SceneManager.GetActiveScene().buildIndex));
     }
 
     public void LoadSceneByIndex(int sceneIndex)
     {
-        Time.timeScale = 1f;
-        SceneManager.LoadScene(sceneIndex);
+        StartCoroutine(LoadScene(sceneIndex));
+    }
+
+    private IEnumerator DelayedFadeIn()
+    {
+        yield return null;
+        yield return null;
+        if (currentFade != null) StopCoroutine(currentFade);
+        currentFade = StartCoroutine(FadeCanvasGroup(canvasGroup, 1f, 0f, fadeDuration));
+    }
+
+    private IEnumerator LoadScene(int sceneIndex)
+    {
+        if (currentFade != null) StopCoroutine(currentFade);
+        currentFade = StartCoroutine(FadeCanvasGroup(canvasGroup, canvasGroup.alpha, 1f, fadeDuration));
+        yield return currentFade;
+
+        loadingText.gameObject.SetActive(true);
+
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneIndex);
+        asyncLoad.allowSceneActivation = false;
+
+        while (asyncLoad.progress < 0.90f)
+        {
+            yield return null;
+        }
+
+        asyncLoad.allowSceneActivation = true;
+
+        yield return null;
+
+        canvasGroup.alpha = 1f;
+        //yield return StartCoroutine(FadeCanvasGroup(canvasGroup, 1f, 0f, fadeDuration));
+    }
+
+    private IEnumerator FadeCanvasGroup(CanvasGroup cg, float start, float end, float duration)
+    {
+        float elapsedTime = 0.0f;
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.unscaledDeltaTime;
+            cg.alpha = Mathf.Lerp(start, end, elapsedTime / duration);
+            yield return null;
+        }
+
+        cg.alpha = end;
     }
 }
