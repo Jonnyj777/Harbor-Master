ea1b9ef2813a3a95cb039b1cae61456b3946d9b9
Andrew Goldstein <fablefox5@gmail.com>
2025-11-30 18:08:36 -0500

    Revert "add new functions specifically for host switching taking in old lobby data"
    This reverts commit 390954812ab4389cd99490bbbd048911eff8960b.

--- Port Authority/Assets/UI/Scripts/SteamLobbyManagerUITest.cs
+++ Port Authority/Assets/UI/Scripts/SteamLobbyManagerUITest.cs
@@ -410,24 +410,6 @@ public class SteamLobbyManagerUITest : MonoBehaviour
 
     }
 
-    public async void HostAfterSwitch(int oldMaxMemberCount, string oldLobbyName)
-    {
-        print("Hosting started...");
-        var isSuccessful = await CreateLobbyAfterSwitch(oldMaxMemberCount, oldLobbyName);
-
-        if (!isSuccessful)
-        {
-            Debug.LogError("Error with creating lobby with steam.");
-            return;
-        }
-
-        //await Task.Delay(5000);
-
-        NetworkManager.singleton.StartHost();
-
-
-    }
-
     public async void GetLobbyInfo() // refresh
     {
 
@@ -653,39 +635,6 @@ public class SteamLobbyManagerUITest : MonoBehaviour
         isLobbySet = true;
         print("setLobby");
     }
-    public async Task<bool> CreateLobbyAfterSwitch(int maxMembers, string lobbyName)
-    {
-        try
-        {
-            var lobbyOutput = await SteamMatchmaking.CreateLobbyAsync();
-            if (!lobbyOutput.HasValue)
-            {
-                Debug.Log("Created but not instantiated");
-                return false;
-            }
-
-            SetLobby(lobbyOutput.Value);
-            Lobby.SetPublic();
-            // Lobby.SetFriendsOnly();
-            Lobby.SetJoinable(true);
-            Lobby.SetData("name", lobbyName);
-            Lobby.SetData("maxMembers", maxMembers.ToString());
-            Lobby.SetData("game", "PORTAUTH");
-            Lobby.SetData("HostAddress", SteamClient.SteamId.Value.ToString());
-            Lobby.SetData("hasStarted", "false");
-            Lobby.SetData("id", lobbyOutput.Value.Id.ToString());
-            SteamLobbyManagerUITest.currentHostID = SteamClient.SteamId;
-
-
-            return true;
-
-        }
-        catch (System.Exception ex)
-        {
-            Debug.LogException(ex);
-            return false;
-        }
-    }
 
     public async Task<bool> CreateLobby()
     {
@@ -854,18 +803,16 @@ public class SteamLobbyManagerUITest : MonoBehaviour
 
     private void HostSwitchToNewHost()
     {
-        print("switching host to new lobby");
         Steamworks.Data.Lobby oldLobby = Lobby;
         LeaveLobby();
         NetworkManager.singleton.StopHost();
-        HostAfterSwitch(int.Parse(oldLobby.GetData("maxMembers")), oldLobby.GetData("name"));
+        Host();
         oldLobby.SetData("switchedLobby", oldLobby.Id.ToString());
         
     }
 
     public IEnumerator ClientSwitchToNewHost()
     {
-        print("switching client to new lobby");
         yield return new WaitForSeconds(1f);
         string newLobby = Lobby.GetData("switchedLobby");
         LeaveLobby();
