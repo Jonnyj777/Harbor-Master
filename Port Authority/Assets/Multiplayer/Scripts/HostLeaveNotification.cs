07bd844cd33ca3a4ea1edce85a334c3f3080b5e5
Andrew Goldstein <fablefox5@gmail.com>
2025-12-01 00:33:50 -0500

    remove server tag

--- Port Authority/Assets/Multiplayer/Scripts/HostLeaveNotification.cs
+++ Port Authority/Assets/Multiplayer/Scripts/HostLeaveNotification.cs
@@ -19,8 +19,6 @@ public class HostLeaveNotification : MonoBehaviour
             Destroy(gameObject);
         }
     }
-
-    [Server]
     public void HostLeft()
     {
         ShowHostLeftNotification();
